bf873879e5d78f44c84b0f687e7d4c6bf963e7a0
Joe Haddad <jsh77@zips.uakron.edu>
2017-01-10 22:02:52 -0500

    Complete some code

--- lib/scggm_soft_threshold.hpp
+++ lib/scggm_soft_threshold.hpp
@@ -5,26 +5,28 @@
 
 scggm_theta scggm_soft_threshold(scggm_theta theta, double c1, double c2) {
   scggm_theta B;
+  SMatrix Bxy = std::make_shared<Matrix>(theta.xy->rows, theta.xy->cols);
   /*
-      Bxy=zeros(size(theta.xy));
-      pos_idx=theta.xy(:)>c1;
-      neg_idx=theta.xy(:)<-c1;
+  TODO
+  pos_idx=theta.xy(:)>c1;
+  neg_idx=theta.xy(:)<-c1;
 
-      Bxy(pos_idx)=theta.xy(pos_idx)-c1;
-      Bxy(neg_idx)=theta.xy(neg_idx)+c1;
-
-      Byy = diag(diag(theta.yy));
-      uyy = triu( theta.yy,1 );
-      pos_idx=uyy(:)>c2;
-      neg_idx=uyy(:)<-c2;
-
-      Byy(pos_idx) = uyy(pos_idx)-c2;
-      Byy(neg_idx) = uyy(neg_idx)+c2;
-      Byy = Byy + Byy'-diag(diag(theta.yy));
-
-      B.xy = (Bxy);
-      B.yy = (Byy);
+  Bxy(pos_idx)=theta.xy(pos_idx)-c1;
+  Bxy(neg_idx)=theta.xy(neg_idx)+c1;
   */
+  SMatrix Byy = theta.yy->diag()->diag();
+  /*
+  TODO
+  uyy = triu( theta.yy,1 );
+  pos_idx=uyy(:)>c2;
+  neg_idx=uyy(:)<-c2;
+
+  Byy(pos_idx) = uyy(pos_idx)-c2;
+  Byy(neg_idx) = uyy(neg_idx)+c2;
+  Byy = Byy + Byy'-diag(diag(theta.yy));
+  */
+  B.xy = Bxy;
+  B.yy = Byy;
   return B;
 }
 
