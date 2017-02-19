20e7d74ce9528e9bb7b4585699667536e9bb0e1e
Joe Haddad <jsh77@zips.uakron.edu>
2017-02-19 00:09:33 -0500

    Keep finding a good init matrix

--- lib/scggm_initialize.hpp
+++ lib/scggm_initialize.hpp
@@ -8,15 +8,17 @@
 
 scggm_theta scggm_initialize(int J, int K) {
   scggm_theta Theta0;
-  SMatrix Ai;
-  if (K <= 100) {
-    Ai = sprandsym(K, 0.01);
-    Theta0.xy = sprand(J, K, 0.01);
-  } else {
-    Ai = sprandsym(K, 0.001);
-    Theta0.xy = sprand(J, K, 0.001);
-  }
-  Theta0.yy = Ai->scalar(0.01) * Ai->transpose() + eye(K, K)->scalar(0.7);
+  do {
+    SMatrix Ai;
+    if (K <= 100) {
+      Ai = sprandsym(K, 0.01);
+      Theta0.xy = sprand(J, K, 0.01);
+    } else {
+      Ai = sprandsym(K, 0.001);
+      Theta0.xy = sprand(J, K, 0.001);
+    }
+    Theta0.yy = Ai->scalar(0.01) * Ai->transpose() + eye(K, K)->scalar(0.7);
+  } while (Theta0.yy->cholesky().error);
   return Theta0;
 }
 
