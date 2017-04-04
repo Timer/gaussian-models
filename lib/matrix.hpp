6cf3cdb30fda430360378db797947f53ac8fb778
Joe Haddad <jsh77@zips.uakron.edu>
2017-04-03 21:14:48 -0400

    Add cuda lgamma

--- lib/matrix.hpp
+++ lib/matrix.hpp
@@ -691,6 +691,21 @@ public:
     return m;
   }
 
+#if ACCELERATE_MODE == ACCELERATE_MODE_CUDA
+  template <class T>
+  __host__ void getLaunchConfiguration(T t, int n, int *blocks, int *threads) {
+    cudaOccupancyMaxPotentialBlockSize(blocks, threads, t, 0, n);
+    *blocks = (n + *threads - 1) / *threads;
+  }
+
+  __global__ void vec_lgamma(double *a, double *c, const unsigned int n) {
+    const long idx = blockIdx.x * blockDim.x + threadIdx.x;
+    if (idx < n) {
+      c[id] = lgamma(a[id]);
+    }
+  }
+#endif
+
 #if ACCELERATE_MODE == ACCELERATE_MODE_OPENCL
   const char *lgammaKernelSource =
       "\n"
@@ -716,9 +731,13 @@ public:
       accelerate();
 
 #if ACCELERATE_MODE == ACCELERATE_MODE_CUDA
+      auto N = rows * cols;
       double *C_accelerate_data = nullptr;
       cudaMalloc(&C_accelerate_data, rows * cols * sizeof(double));
-      //TODO: this
+      int blocks, threads;
+      getLaunchConfiguration(vec_lgamma, N, &blocks, &threads);
+      vec_lgamma<<<blocks, threads>>>(accelerate_data, C_accelerate_data, N);
+      cudaDeviceSynchronize();
       C->inherit(C_accelerate_data);
 #elif ACCELERATE_MODE == ACCELERATE_MODE_OPENCL
       auto N = rows * cols;
