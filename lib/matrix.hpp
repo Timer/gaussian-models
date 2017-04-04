c723951e352bf3e9fec360ad44ce50284d7e0e2f
Joe Haddad <jsh77@zips.uakron.edu>
2017-04-03 21:20:22 -0400

    move

--- lib/matrix.hpp
+++ lib/matrix.hpp
@@ -25,6 +25,21 @@
 #include <cuda_runtime.h>
 #endif
 
+#if ACCELERATE_MODE == ACCELERATE_MODE_CUDA
+template <class T>
+__host__ void getLaunchConfiguration(T t, int n, int *blocks, int *threads) {
+  cudaOccupancyMaxPotentialBlockSize(blocks, threads, t, 0, n);
+  *blocks = (n + *threads - 1) / *threads;
+}
+
+__global__ void vec_lgamma(double *a, double *c, const unsigned int n) {
+  const long idx = blockIdx.x * blockDim.x + threadIdx.x;
+  if (idx < n) {
+    c[id] = lgamma(a[id]);
+  }
+}
+#endif
+
 class Matrix;
 typedef std::shared_ptr<Matrix> SMatrix;
 
@@ -690,21 +705,6 @@ public:
     }
     return m;
   }
-
-#if ACCELERATE_MODE == ACCELERATE_MODE_CUDA
-  template <class T>
-  __host__ void getLaunchConfiguration(T t, int n, int *blocks, int *threads) {
-    cudaOccupancyMaxPotentialBlockSize(blocks, threads, t, 0, n);
-    *blocks = (n + *threads - 1) / *threads;
-  }
-
-  __global__ void vec_lgamma(double *a, double *c, const unsigned int n) {
-    const long idx = blockIdx.x * blockDim.x + threadIdx.x;
-    if (idx < n) {
-      c[id] = lgamma(a[id]);
-    }
-  }
-#endif
 
 #if ACCELERATE_MODE == ACCELERATE_MODE_OPENCL
   const char *lgammaKernelSource =
