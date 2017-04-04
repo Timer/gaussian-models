7f8f1a59b2f739149a477a156a95890e4d60c44e
Joe Haddad <jsh77@zips.uakron.edu>
2017-04-03 21:32:10 -0400

    Cst

--- lib/cumatrix.cu
+++ lib/cumatrix.cu
@@ -18,7 +18,7 @@ __global__ void vec_lgamma(double *a, double *c, const unsigned int n) {
 __host__ double *cu_lgammed(const int rows, const int cols, double *oData) {
   auto N = rows * cols;
   double *C_accelerate_data = nullptr;
-  cudaMalloc(&C_accelerate_data, rows * cols * sizeof(double));
+  cudaMalloc((void **) &C_accelerate_data, rows * cols * sizeof(double));
   int blocks, threads;
   getLaunchConfiguration(vec_lgamma, N, &blocks, &threads);
   vec_lgamma<<<blocks, threads>>>(oData, C_accelerate_data, N);
